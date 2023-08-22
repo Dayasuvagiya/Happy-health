036d6679031f5dea296c6d99333cc85738b87e89
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-08-22 15:59:47 +0200

    Add name atribute and fetch data in views file

--- employe/views.py
+++ employe/views.py
@@ -8,6 +8,12 @@ def employe_home(request):
 def add_employe(request):
     if request.method=="POST":
         # Data Fetch
+        employee_name=request.POST.get("employee_name")
+        employee_id=request.POST.get("employee_id")
+        employee_phone=request.POST.get("employee_phone")
+        employee_address=request.POST.get("employee_address")
+        employee_working=request.POST.get("employee_working")
+        employee_department=request.POST.get("employee_department")
 
         # Create model object and set data
 
