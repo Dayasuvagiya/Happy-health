98872af65e481645f501c32b002e0d75fb62a3ac
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-08-23 00:23:43 +0200

    Created update curd functionality

--- employe/urls.py
+++ employe/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("home/", views.employe_home, name="home"),
     path("add-employe/", views.add_employe, name="employe"),
     path("delete-employe/<int:employe_id>", views.delete_employe, name="delete"),
+    path("update-employe/<int:employe_id>", views.update_employe, name="update"),
+    path("do-update-emp/<int:employe_id>", views.do_update_emp, name="doupdate"),
 ]
--- employe/views.py
+++ employe/views.py
@@ -19,7 +19,6 @@ def add_employe(request):
         employee_address=request.POST.get("employee_address")
         employee_working=request.POST.get("employee_working")
         employee_department=request.POST.get("employee_department")
-        print("phone", employee_phone, request)
 
         # Create model object and set data
         e=Emp()
@@ -46,4 +45,34 @@ def add_employe(request):
 def delete_employe(request,employe_id):
     employe=Emp.objects.get(pk=employe_id)
     employe.delete()
+    return redirect("/employe/home/")
+
+
+def update_employe(request,employe_id):
+    employe=Emp.objects.get(pk=employe_id)
+    return render(request,"employe/update_employe.html",{
+        'employe':employe
+    })
+
+def do_update_emp(request,employe_id):
+    if request.method=='POST':
+        employee_name=request.POST.get("employee_name")
+        employee_id_temp=request.POST.get("employee_id")
+        employee_phone=request.POST.get("employee_phone")
+        employee_address=request.POST.get("employee_address")
+        employee_working=request.POST.get("employee_working")
+        employee_department=request.POST.get("employee_department")
+
+        e=Emp.objects.get(pk=employe_id)
+        e.name=employee_name
+        e.emp_id=employee_id_temp
+        e.phone=employee_phone
+        e.address=employee_address
+        e.department=employee_department
+        if employee_working is None:
+            e.working=False
+        else:
+            e.working=True
+        e.save()
+
     return redirect("/employe/home/")
