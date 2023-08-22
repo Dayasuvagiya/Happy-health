24e5213a6fd4ba08429adf4221808f6f14aa8a31
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-08-22 15:51:14 +0200

    Added post method to submit form

--- employe/views.py
+++ employe/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
@@ -6,4 +6,15 @@ def employe_home(request):
     return render(request, "employe/home.html",{})
 
 def add_employe(request):
+    if request.method=="POST":
+        # Data Fetch
+
+        # Create model object and set data
+
+        # Save the object
+
+        # Prepare msg
+
+
+        return redirect("/employe/home/")
     return render(request, "employe/add_employe.html",{})
