a2ee336c323d643a358dcacbea53ff1112e29cc6
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-08-23 11:10:11 +0200

    -Set data in admin file -Added add new employee button

--- employe/models.py
+++ employe/models.py
@@ -8,3 +8,6 @@ class Emp(models.Model):
     address=models.CharField(max_length=200)
     working=models.BooleanField(default=True)
     department=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
