6ad9ef6970b2c134a45d74c82b54a147681ef61f
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-08-28 22:52:36 +0200

    Home page design complated

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-STATIC_URL = '/static/'
 # Application definition
 
 INSTALLED_APPS = [
@@ -137,9 +136,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 BASE_URL = 'http://localhost:8000/'
+
+
