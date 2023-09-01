9406b2d6395bf25e0b9133e66077afd217856bf7
Daya Suvagiya <diya.jivani.diji@gmail.com>
2023-09-02 00:38:44 +0200

    Create 403, 404, 500 error page

--- employe/urls.py
+++ employe/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from employe import views
+from .views import handler404, handler500, handler403
 
 urlpatterns = [
     path("home/", views.employe_home, name="home"),
@@ -10,3 +11,8 @@ urlpatterns = [
     path("do-update-emp/<int:employe_id>", views.do_update_emp, name="update-emp"),
     path("contact/", views.contact, name="contact"),
 ]
+
+
+handler404 = handler404
+handler500 = handler500
+handler403 = handler403
--- employe/views.py
+++ employe/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Emp,Userlogin,Contactus
 from .form import ContactForm
+from django.core.exceptions import PermissionDenied
 
 # employee home page view
 def employe_home(request):
@@ -224,4 +225,27 @@ def about(request):
     print=("<h1>This is about page</h1>")
     return render(request,"about.html",{})
 
+
+
+""" Error handling """
+
+
+def handler404(request, exception):
+    """Function to render the 404 page."""
+    return render(request, '404.html', status=404)
+
+
+def handler500(request):
+    """ Function to render the 500 page."""
+    return render(request, '500.html', status=500)
+
+
+def handler403(request, exception):
+    """Function to render the 403 page."""
+    if isinstance(exception, PermissionDenied):
+        return render(request, '403.html', status=403)
+    else:
+        # Handle unexpected errors with a generic 500 error page
+        return render(request, '500.html', status=500)
+    
 
